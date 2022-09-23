f7c087ccddd16c54535da39a5e27dbd6c20c927e
christiaanlok <61160959+christiaanlok@users.noreply.github.com>
2022-09-23 22:30:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -737,5 +737,5 @@ def update_table(start, end, clinic, admit_type, heatmap_click, reset_click, *ar
 
 
 # Run the server
-if __name__ == "__main__":
-    app.run_server(debug=True)
+if __name__ == '__main__':
+     app.run_server(host="0.0.0.0", debug=True, port=8080)
